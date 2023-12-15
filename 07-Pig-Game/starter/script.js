07264d8ff5dcf322da0712832aedc9d7fb5f9ad0
Divya <divya.sridurga5@gmail.con>
2023-12-15 19:00:45 +0530

    click implementation

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -6,23 +6,42 @@
 // 5 if not add the current score
 
 let diceRoll = document.querySelector('.btn--roll');
-let diceimage= document.querySelector('.dice');
-let playerOne= document.querySelector('.player--0');
-let playerTwo= document.querySelector('.player--1');
-let currentScore=0;
+let diceimage = document.querySelector('.dice');
+let playerOne = document.querySelector('.player--0');
+let playerTwo = document.querySelector('.player--1');
+let playerCurrentScore1 = document.getElementById("current--0");
+let playerOneInitialScore = document.getElementById('score--0');
+let playerTwoInitialScore = document.getElementById('score--1');
+let buttonHold=document.querySelector('.btn--hold')
+
+let currentScore = 0;
+let activePlayer = 0;
+playerOneInitialScore.textContent = 0;
+playerTwoInitialScore.textContent = 0;
 diceimage.classList.add('hidden')
 
 diceRoll.addEventListener('click', function () {
-    let number = Math.trunc(Math.random() * 6)+1;
+    let number = Math.trunc(Math.random() * 6) + 1;
     diceimage.classList.remove('hidden')
     // diceimage.setAttribute('src',`dice-${number}.png`);
-    diceimage.src= `dice-${number}.png`;
-    if(number !==1){
+    diceimage.src = `dice-${number}.png`;
+    if (number !== 1) {
         // add to the current score
-        currentScore= currentScore+number;
-        document.querySelector('.current-score').textContent=currentScore;
+        currentScore = currentScore + number;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+        // reset the current score
+        currentScore = 0;
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        playerOne.classList.toggle('player--active')
+        playerTwo.classList.toggle('player--active')
 
-    }else{
-        // switch the user
     }
 })
+
+buttonHold.addEventListener('click',function(){
+
+})
+
+
