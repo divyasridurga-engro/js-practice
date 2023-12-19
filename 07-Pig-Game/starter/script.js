d6f96f06fdda17c2863e50a3b26414ea0a06c21c
Divya Sri <divyasridurga.t@engro.io-at-491938176112>
2023-12-19 06:55:52 +0530

    pig game final commit

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,15 +1,8 @@
 'use strict';
-// on clicking a roll a dice =
-// 1. generate a random number =
-// 2. update the image =
-//  3. if 1 switch player
-// 5 if not add the current score
-
 let diceRoll = document.querySelector('.btn--roll');
 let diceimage = document.querySelector('.dice');
 let playerOne = document.querySelector('.player--0');
 let playerTwo = document.querySelector('.player--1');
-let playerCurrentScore1 = document.getElementById("current--0");
 let playerOneInitialScore = document.getElementById('score--0');
 let playerTwoInitialScore = document.getElementById('score--1');
 let buttonHold=document.querySelector('.btn--hold');
@@ -32,48 +25,50 @@ function switchPlayer(){
 }
 
 diceRoll.addEventListener('click', function () {
-    if(isplaying){
-        let number = Math.trunc(Math.random() * 6) + 1;
-    diceimage.classList.remove('hidden')
-    // diceimage.setAttribute('src',`dice-${number}.png`);
+  if (isplaying) {
+    // rolling a dice
+    let number = Math.trunc(Math.random() * 6) + 1;
+    // remove the hidden class of dice (to make dice visible onclick)
+    diceimage.classList.remove('hidden');
+    // updating the dice image
     diceimage.src = `dice-${number}.png`;
+    // if number is not 1 update the current score else switch player
     if (number !== 1) {
-        // add to the current score
-        currentScore = currentScore + number;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+      currentScore = currentScore + number;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
     } else {
-        // reset the current score
-        switchPlayer()
-
+      switchPlayer();
     }
-    }
-})
+  }
+});
+// button hold functionality
 buttonHold.addEventListener('click',function(e){
    if(isplaying){
-     // add the current score to the total score
+    // updating the player score on clicking hold
      scores[activePlayer]+=currentScore;
      document.getElementById(`score--${activePlayer}`).textContent=scores[activePlayer];
-     
-     // check if the score is greater than 100
-     if(scores[activePlayer]>=10){
+    //  checking whether the score is greater than 100 and adding the player--winner class
+     if(scores[activePlayer]>=100){
          document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
          document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
-         isplaying=false
+         isplaying=false   // to stop the game after winning
      }
      else{
          switchPlayer();
      }
    }
 })
-
+// resetting the game to initial state
 buttonNewGame.addEventListener('click', function(){
     currentScore=0
     isplaying=true
+    scores=[0,0]
     document.getElementById(`current--0`).textContent = 0;
     document.getElementById(`current--1`).textContent = 0;
     diceimage.classList.add('hidden');
-    document.getElementById(`score--0`).textContent=0;
-    document.getElementById(`score--1`).textContent=0;
+    playerOneInitialScore.textContent=0;
+    playerTwoInitialScore.textContent=0;
     document.querySelector('.player--0').classList.remove('player--winner')
     document.querySelector('.player--0').classList.add('player--active')
     document.querySelector('.player--1').classList.remove('player--winner')
